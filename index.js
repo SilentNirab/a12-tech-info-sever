f9ddfc9897f8e911d52afe241bef7a1ad0ceea4f
Md Nirab <nabilnirab255@gmail.com>
2023-11-28 23:36:31 +0600

    get data from DB

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER,process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ezxu64p.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,12 +25,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const productCollection = client.db("tech-info").collection("products");
+        const userCollection = client.db("tech-info").collection("users");
+
+        //Product related api 
+        app.get('/products', async(req, res) => {
+            const result = await productCollection.find().toArray()
+            res.send(result)
+        })
+
+        //User related api 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+              return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+          });
+        
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
@@ -39,9 +59,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('tech-info is sitting')
+    res.send('tech-info is runing')
 })
 
 app.listen(port, () => {
-    console.log(`tech=info is sitting on port ${port}`);
+    console.log(`tech=info is runing on port ${port}`);
 })
